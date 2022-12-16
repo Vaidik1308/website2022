757925820222bc93ee9f7beeaba9abe979efc7e5
Vaidik Singh Nirwan <vaidiksinghnirwan.com>
2022-12-16 20:22:31 +0530

    Your message

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <div className='Header'>
+        <img className='image' src='https://lineageos.org/assets/img/longevity.jpg' alt="no" />
+        <div className='para'>
+            <h1>Longevity</h1>
+            <p>LineageOS extends the functionality and lifespan of mobile devices from more than 20 different manufacturers thanks to our open-source community of contributors from all around the world.</p>
+            <p>Want to contribute? See how you can make LineageOS better.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
